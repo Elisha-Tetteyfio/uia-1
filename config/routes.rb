9803f2d6f0c98d0afc66609019756e20bab001fe
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-11-23 10:40:08 +0000

    Add predictor endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/users/predictor", to: "users#predictor"
+
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
